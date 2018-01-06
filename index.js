08ce8b325b2e6aab409dab6e735885a70d625bdd
sssmi <simeon.kerkola@gmail.com>
2018-01-06 21:24:27 +0200

    Add ability to login with Google

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
+require('./models/User');
 require('./services/passport');
 
 mongoose.connect(process.env.MONGODB_URI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    keys: [process.env.COOKIE_KEY], // Possibility for multiple keys for security
+  }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authRoutes')(app); // same as authRoutes(app)
 
 const PORT = process.env.PORT || 3000;
--- services/passport.js
+++ services/passport.js
@@ -1,5 +1,18 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
+
+// Stuff user info to a cookie
+passport.serializeUser((user, done) => {
+  done(null, user.id); // user.id is mongo's _id
+});
+
+// Get the user ID from the cookie
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => done(null, user));
+});
 
 passport.use(
   new GoogleStrategy(
@@ -9,10 +22,16 @@ passport.use(
       callbackURL: '/auth/google/callback', // This uri and the Authorized redirect URIs at google console has to match
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken', accessToken);
-      console.log('refreshToken', refreshToken);
-      console.log('profile', profile);
-      done();
+      // Look for a existing user
+      User.findOne({ googleId: profile.id }).then((exsistingUser) => {
+        if (exsistingUser) {
+          done(null, exsistingUser); // 1st arg: if error, run this, 2nd arg: User Record
+        }
+        // Create a new user if not found
+        new User({ googleId: profile.id }).save().then(
+          user => done(null, user), // Return the new user
+        );
+      });
     },
   ),
 );
