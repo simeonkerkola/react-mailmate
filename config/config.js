8840432490fdd33595916f7a9bb32ea8f06df997
sssmi <simeon.kerkola@gmail.com>
2018-01-07 21:40:55 +0200

    Separate dev and prod environments

--- config/config.js
+++ config/config.js
@@ -2,12 +2,11 @@ const env = process.env.NODE_ENV || 'development';
 
 console.log('env *****', env);
 
-if (env === 'development' || env === 'test') {
-  const config = require('./config.json'); // eslint-disable-line global-require
-  const envConfig = config[env];
+const config = require('./config.json');
+// eslint-disable-line global-require
+const envConfig = config[env];
 
-  // Object.keys() returns an array of all the keys
-  Object.keys(envConfig).forEach((key) => {
-    process.env[key] = envConfig[key];
-  });
-}
+// Object.keys() returns an array of all the keys
+Object.keys(envConfig).forEach((key) => {
+  process.env[key] = envConfig[key];
+});
