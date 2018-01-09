2ae862e8cf95840153d28d2a2ee2bcaf62e3c801
sssmi <simeon.kerkola@gmail.com>
2018-01-09 23:37:47 +0200

    Add create-react-app front end

--- index.js
+++ index.js
@@ -23,5 +23,5 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app); // same as authRoutes(app)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('app running on a port:', PORT));
