aa3c892eedcc75fa16f11ac5f97b34fe2cbc156a
sssmi <simeon.kerkola@gmail.com>
2018-01-14 08:39:40 +0200

    Add React Router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
 
-const App = () => <div>Hi There!</div>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Route path="/" component={Landing} exact />
+        <Route path="/surveys" component={Dashboard} exact />
+        <Route path="/surveys/new" component={SurveyNew} />
+      </div>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
