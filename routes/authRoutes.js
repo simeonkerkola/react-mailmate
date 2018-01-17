59eeecc2eb3174f9f8a0e9af9f4e352d5c2a9aa2
sssmi <simeon.kerkola@gmail.com>
2018-01-17 17:53:51 +0200

    Redirect user on Logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -18,7 +18,7 @@ module.exports = (app) => {
 
   app.get('/api/logout', (req, res) => {
     req.logout(); // Logs out and kills the cookie
-    res.send(req.user);
+    res.redirect('/');
   });
 
   app.get('/api/current_user', (req, res) => {
