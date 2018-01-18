f6d82ecce097912aa216441c304471e8a41703ba
sssmi <simeon.kerkola@gmail.com>
2018-01-18 22:03:04 +0200

    Lint

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,13 +3,8 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
-  renderContent = () => {
-    return this.props.auth ? (
-      <a href="/api/logout">Logout</a>
-    ) : (
-      <a href="/auth/google">Login</a>
-    );
-  };
+  renderContent = () =>
+    this.props.auth ? <a href="/api/logout">Logout</a> : <a href="/auth/google">Login</a>;
   render() {
     return (
       <div>
@@ -18,7 +13,8 @@ class Header extends React.Component {
             <Link
               to={this.props.auth ? '/surveys' : '/'}
               className="brand-logo"
-              style={{ paddingLeft: '.75rem' }}>
+              style={{ paddingLeft: '.75rem' }}
+            >
               mailmate
             </Link>
             <ul id="nav-mobile" className="right">
