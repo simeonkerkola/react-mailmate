5376d65f98aaa2b197578a2997c84607dc76486f
sssmi <simeon.kerkola@gmail.com>
2018-01-21 14:16:34 +0200

    Add handleToken Action

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+
+import * as actions from '../actions';
 
 class Payments extends React.Component {
   render() {
@@ -8,7 +11,10 @@ class Payments extends React.Component {
         name="Mailmate"
         description="$5 for 5 emails"
         amount={500} // cents
-        token={token => console.log(token)} // token we receive from stripe
+        token={(token) => {
+          console.log('props', this.props);
+          return this.props.handleToken(token);
+        }} // token we receive from stripe
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
       >
         {/* Add a button as a child component */}
@@ -18,4 +24,5 @@ class Payments extends React.Component {
   }
 }
 
-export default Payments;
+// mapDispatchToProps
+export default connect(null, actions)(Payments);
