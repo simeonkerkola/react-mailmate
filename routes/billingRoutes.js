0c7e9934cc7e6d499bbc346831cccc161803d5fd
sssmi <simeon.kerkola@gmail.com>
2018-01-23 10:37:32 +0200

    Add middeware to check if user is logged in

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,9 +1,10 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = (app) => {
-  app.post('/api/stripe', async (req, res) => {
+  // run requireLogin middleware whenever user tries to make a post request
+  app.post('/api/stripe', requireLogin, async (req, res) => {
     // console.log(req.body);
-
     await stripe.charges.create({
       amount: 500,
       currency: 'usd',
