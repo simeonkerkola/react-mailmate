ea57329a289496c99ea626a4ed3ce5160bb4c714
sssmi <simeon.kerkola@gmail.com>
2018-02-02 21:36:34 +0200

    Setup Sendgrid Mailer

--- services/Mailer.js
+++ services/Mailer.js
@@ -2,4 +2,15 @@ const sendgrid = require('sendgrid');
 
 const helper = sendgrid.mail;
 
-class Mailer extends helper.Mail {}
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
+
+    this.from_email = new helper.Email('no-reply@mailmate.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html, content');
+    this.recipients = this.formatAddresses(recipients);
+  }
+}
+
+module.exports = Mailer;
