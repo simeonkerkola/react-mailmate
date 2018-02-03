30d2b3567baf58b647e695ddb15e24842e18e7b7
sssmi <simeon.kerkola@gmail.com>
2018-02-03 20:40:08 +0200

    Edit the email template

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,1 +1,18 @@
-module.exports = survey => `<div>${survey.body}</div>`;
+module.exports = survey => `
+  <html>
+    <body>
+      <div style="text-align: center;">
+        <h1>We'd like your input!</h1>
+        <h3>Please answer the following question.</h3>
+        <p>${survey.body}</p>
+        <div>
+          <a href="http://localhost:3000">Yes</a>
+        </div>
+        <div>
+          <a href="http://localhost:3000">No</a>
+        </div>
+      </div>
+    </body>
+  </html>
+
+    `;
